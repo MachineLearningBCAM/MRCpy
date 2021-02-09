46562522ecc37713b5157692e9a964e27a2df805
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-02-09 19:46:37 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,70 @@
+
+"""Create instructions to build the MRC package."""
+
+import os
+import runpy
+from setuptools import find_packages, setup
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+minimax_risk_classifiers = runpy.run_path(os.path.join(base_dir, 'minimax_risk_classifiers', '__init__.py'))
+
+def parse_requirements_file(filename):
+    """
+    Read the lines of the requirements file.
+
+    Parameters
+    ----------
+    filename : str
+        The filename to read
+
+    Returns
+    -------
+        Array of lines of the requirements file.
+    """
+    with open(filename) as input_file:
+        return input_file.read().splitlines()
+
+if __name__ == '__main__':
+
+    requirements = parse_requirements_file('requirements.txt')
+    install_requires = []
+
+    # Add all the requirements.
+    for requirement in requirements:
+        install_requires.append(requirement)
+
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    setup(
+        name="minimax_risk_classifiers",
+        version=minimax_risk_classifiers['__version__'],
+        install_requires=install_requires,
+        description="Minimax risk classification",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/MachineLearningBCAM/Minimax-Risk-Classifiers",
+        packages=find_packages(),
+        # py_modules=["MRC", "CMRC", "phi.Phi", \
+                    # "phi.PhiGaussian", "phi.PhiLinear", "phi.PhiThreshold"],
+        # package_dir={'': 'minimax_risk_classifiers'},
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Machine Learning",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8"
+        ],
+        data_files=[
+            "README.md"
+        ],
+        include_package_data=True,
+        package_data={'': ['datasets/data/*', 'datasets/descr/*']},
+        python_requires='>=3.6',
+        zip_safe=False
+    )
