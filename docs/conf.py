74caad77d6ca4c7052fd40808aa9720ae960a2d8
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-03-10 11:39:25 +0100

    updating the files

---
+++ docs/conf.py
@@ -0,0 +1,73 @@
+"""Sphinx configuration file."""
+
+import geomstats
+
+
+project = 'Geomstats'
+copyright = '2019-2020, Geomstats, Inc.'
+author = 'Geomstats Team'
+release = version = geomstats.__version__
+
+extensions = [
+    'nbsphinx',
+    'nbsphinx_link',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+# Configure napoleon for numpy docstring
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = False
+napoleon_use_rtype = False
+napoleon_include_init_with_doc = False
+
+# Configure nbsphinx for notebooks execution
+nbsphinx_execute = 'never'
+nbsphinx_allow_errors = True
+
+templates_path = ['_templates']
+
+source_suffix = ['.rst', '.ipynb']
+
+master_doc = 'index'
+
+language = None
+
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = None
+
+html_theme = 'sphinx_rtd_theme'
+html_baseurl = 'geomstats.github.io'
+htmlhelp_basename = 'geomstatsdoc'
+html_last_updated_fmt = '%c'
+
+latex_elements = {
+}
+
+
+latex_documents = [
+    (master_doc, 'geomstats.tex', 'geomstats Documentation',
+     'Geomstats Team', 'manual'),
+]
+
+man_pages = [
+    (master_doc, 'geomstats', 'geomstats Documentation',
+     [author], 1)
+]
+
+texinfo_documents = [
+    (master_doc, 'geomstats', 'geomstats Documentation',
+     author, 'geomstats', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+epub_title = project
+epub_exclude_files = ['search.html']
