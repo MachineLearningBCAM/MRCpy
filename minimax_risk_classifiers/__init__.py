67237e86adb3707a0e77a1e94a51b53b5a7df151
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-03-17 10:53:41 +0100

    removing old files

--- minimax_risk_classifiers/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '1.0.1'
-
-from minimax_risk_classifiers._MRC_ import _MRC_
-from minimax_risk_classifiers.MRC import MRC
-from minimax_risk_classifiers.CMRC import CMRC
--- minimax_risk_classifiers/phi/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Feature mapping functions used in MRC """
-from minimax_risk_classifiers.phi.phi_gaussian import PhiGaussian
-from minimax_risk_classifiers.phi.phi_linear import PhiLinear
-from minimax_risk_classifiers.phi.phi_threshold import PhiThreshold
-from minimax_risk_classifiers.phi.phi import Phi
