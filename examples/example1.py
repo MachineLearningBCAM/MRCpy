e2fbd5f80d21835fbef7da528619b275e16604d1
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-03-17 11:05:50 +0100

    refactor rename and improve travis ci script

--- examples/example1.py
+++ examples/example1.py
@@ -1,95 +1,103 @@
 """
 Example of using MRC with some of the common classification datasets.
-
 """
 
 import numpy as np
 import time
-
 from sklearn.model_selection import StratifiedKFold
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 
-from minimax_risk_classifiers.MRC import MRC
+from MRCpy import MRC
+# Import the datasets
+from MRCpy.datasets import *
 
-#import the datasets
-from datasets import *
+# Data sets
+loaders = [load_mammographic, load_haberman,
+           load_indian_liver, load_diabetes, load_credit]
+dataName = ["mammographic", "haberman",
+            "indian_liver", "diabetes", "credit"]
 
-#data sets
-loaders = [load_mammographic, load_haberman, load_indian_liver, load_diabetes, load_credit]
-dataName= ["mammographic", "haberman", "indian_liver", "diabetes", "credit"]
 
 def runMRC(phi, loss):
 
-	random_seed = 0
-	res_mean = np.zeros(len(dataName))
-	res_std = np.zeros(len(dataName))
-	np.random.seed(random_seed)
+    random_seed = 0
+    res_mean = np.zeros(len(dataName))
+    res_std = np.zeros(len(dataName))
+    np.random.seed(random_seed)
 
-	# Iterate through each of the dataset and fit the MRC classfier.
-	for j, load in enumerate(loaders):
+    # Iterate through each of the dataset and fit the MRC classfier.
+    for j, load in enumerate(loaders):
 
-		# Loading the dataset
-		X, Y = load(return_X_y=True)
-		r = len(np.unique(Y))
-		n, d= X.shape
+        # Loading the dataset
+        X, Y = load(return_X_y=True)
+        r = len(np.unique(Y))
+        n, d = X.shape
 
-		# Print the dataset name
-		print(" ############## \n" + dataName[j] + " n= " + str(n) + " , d= " + str(d) + ", cardY= "+ str(r))
+        # Print the dataset name
+        print(" ############## \n" + dataName[j] + " n= " + str(n) +
+              " , d= " + str(d) + ", cardY= " + str(r))
 
-		clf = MRC(n_classes=r, phi=phi, loss=loss, max_iters=10000)
+        clf = MRC(n_classes=r, phi=phi, loss=loss,
+                  use_cvx=True, max_iters=10000, s=0.3)
 
-		# Preprocess
-		trans = SimpleImputer(strategy='median')
-		X = trans.fit_transform(X, Y)
+        # Preprocess
+        trans = SimpleImputer(strategy='median')
+        X = trans.fit_transform(X, Y)
 
-		# Generate the partitions of the stratified cross-validation
-		cv = StratifiedKFold(n_splits=10, random_state=random_seed, shuffle=True)
+        # Generate the partitions of the stratified cross-validation
+        cv = StratifiedKFold(n_splits=10, random_state=random_seed,
+                             shuffle=True)
 
-		np.random.seed(random_seed)
-		cvError= list()
-		auxTime= 0
-		upper= 0
-		lower= 0
+        np.random.seed(random_seed)
+        cvError = list()
+        auxTime = 0
+        upper = 0
+        lower = 0
 
-		# Paired and stratified cross-validation
-		for train_index, test_index in cv.split(X, Y):
+        # Paired and stratified cross-validation
+        for train_index, test_index in cv.split(X, Y):
 
-			X_train, X_test = X[train_index], X[test_index]
-			y_train, y_test = Y[train_index], Y[test_index]
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = Y[train_index], Y[test_index]
 
-			# Normalizing the data
-			std_scale = preprocessing.StandardScaler().fit(X_train, y_train)
-			X_train = std_scale.transform(X_train)
-			X_test = std_scale.transform(X_test)
+            # Normalizing the data
+            std_scale = preprocessing.StandardScaler().fit(X_train, y_train)
+            X_train = std_scale.transform(X_train)
+            X_test = std_scale.transform(X_test)
 
-			# Save start time for computing training time
-			startTime= time.time()
+            # Save start time for computing training time
+            startTime = time.time()
 
-			clf.fit(X_train, y_train)
-			upper += clf.upper_
-			lower += clf.getLowerBound()
+            clf.fit(X_train, y_train)
+            upper += clf.upper_
+            lower += clf.getLowerBound()
+            print('The zero mu\'s are : ', np.sum(clf.mu_ == 0))
 
-			# Calculate the training time
-			auxTime+= time.time() - startTime
+            # Calculate the training time
+            auxTime += time.time() - startTime
 
-			y_pred= clf.predict(X_test)
-			cvError.append(np.average(y_pred != y_test))
+            y_pred = clf.predict(X_test)
+            cvError.append(np.average(y_pred != y_test))
 
-		res_mean[j] = np.average(cvError)
-		res_std[j] = np.std(cvError)
+        res_mean[j] = np.average(cvError)
+        res_std[j] = np.std(cvError)
 
-		print(" error= " + ":\t" + str(res_mean[j]) + "\t+/-\t" + str(res_std[j]) + "\n")
-		print(" upper= " + str(upper/10)+"\t lower= " + str(lower/10) + 
-			"\navg_train_time= " + ":\t" + str(auxTime/10) + ' secs' + 
-			"\n ############## \n")
+        print(" error= " + ":\t" + str(res_mean[j]) + "\t+/-\t" +
+              str(res_std[j]) + "\n")
+        print(" upper= " + str(upper / 10) + "\t lower= " + str(lower / 10) +
+              "\navg_train_time= " + ":\t" + str(auxTime / 10) + ' secs' +
+              "\n ############## \n")
+
 
 if __name__ == '__main__':
 
-	print('******************** Example 1 (MRC with default constraints) ********************** \n\n')
+    print('******************** \
+          Example 1 (MRC with default constraints) \
+          ********************** \n\n')
 
-	print('\t\t 1. Using 0-1 loss and threshold feature mapping\n\n')
-	runMRC(phi='threshold', loss='0-1')
+    print('\t\t 1. Using 0-1 loss and threshold feature mapping \n\n')
+    runMRC(phi='gaussian', loss='0-1')
 
-	print('\t\t 2. Using log loss and threshold feature mapping\n\n')
-	runMRC(phi='threshold', loss='log')
+    print('\t\t 2. Using log loss and threshold feature mapping \n\n')
+    runMRC(phi='gaussian', loss='log')
--- examples/example4.py
+++ examples/example4.py
@@ -1,40 +1,34 @@
 """
 Simple example of using CMRC with 0-1 loss.
-
 """
 
 import numpy as np
-import time
-
-from sklearn.model_selection import StratifiedKFold
 from sklearn.impute import SimpleImputer
-from sklearn import preprocessing
 
-from minimax_risk_classifiers.CMRC import CMRC
-
-#import the datasets
-from datasets import load_mammographic
+from MRCpy import CMRC
+# Import the datasets
+from MRCpy.datasets import load_mammographic
 
 if __name__ == '__main__':
 
-	# Loading the dataset
-	X, Y = load_mammographic(return_X_y=True)
-	r = len(np.unique(Y))
+    # Loading the dataset
+    X, Y = load_mammographic(return_X_y=True)
+    r = len(np.unique(Y))
 
-	# Preprocess
-	trans = SimpleImputer(strategy='median')
-	X = trans.fit_transform(X, Y)
+    # Preprocess
+    trans = SimpleImputer(strategy='median')
+    X = trans.fit_transform(X, Y)
 
-	# Fit the MRC model
-	clf = CMRC(n_classes=r).fit(X, Y)
+    # Fit the MRC model
+    clf = CMRC(n_classes=r).fit(X, Y)
 
-	# Prediction
-	print('\n\nThe predicted values for the first 3 instances are : ')
-	print(clf.predict(X[:3, :]))
+    # Prediction
+    print('\n\nThe predicted values for the first 3 instances are : ')
+    print(clf.predict(X[:3, :]))
 
-	# Predicted probabilities
-	print('\n\nThe predicted probabilities for the first 3 instances are : ')
-	print(clf.predict_proba(X[:3, :]))
+    # Predicted probabilities
+    print('\n\nThe predicted probabilities for the first 3 instances are : ')
+    print(clf.predict_proba(X[:3, :]))
 
-	print('\n\nThe score is : ')
-	print(clf.score(X, Y))
+    print('\n\nThe score is : ')
+    print(clf.score(X, Y))
