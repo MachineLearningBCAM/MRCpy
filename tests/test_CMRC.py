754e51987966ae01f9fa65b5b9084e3f43f0a887
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-03-19 16:30:05 +0100

    updating tests

--- tests/test_CMRC.py
+++ tests/test_CMRC.py
@@ -22,55 +22,37 @@ class TestCMRC(unittest.TestCase):
         self.assertTrue(hasattr(clf, 'is_fitted_'))
         self.assertTrue(clf.is_fitted_)
 
+        # Predict the probabilities for each class for the given instances.
+        if loss == 'log':
+            hy_x = clf.predict_proba(self.X)
+            self.assertTrue(hy_x.shape == (self.X.shape[0], r))
+            self.assertTrue(np.all(np.sum(hy_x, axis=1)==1))
+
         y_pred = clf.predict(self.X)
         self.assertTrue(y_pred.shape == (self.X.shape[0],))
 
     # Without using cvxpy
-    # Training test for MRC with 0-1 loss and threshold features.
-    def test_MRC0_1_with_threshold(self):
+    # Training test for CMRC with 0-1 loss.
+    def test_CMRC0_1(self):
         self.CMRC_training(phi='threshold', loss='0-1', use_cvx=False)
-
-    # Training test for MRC with 0-1 loss and linear kernel.
-    def test_MRC0_1_with_linear(self):
         self.CMRC_training(phi='linear', loss='0-1', use_cvx=False)
-
-    # Training test for MRC with 0-1 loss and gaussian kernel.
-    def test_MRC0_1_with_gaussian(self):
         self.CMRC_training(phi='gaussian', loss='0-1', use_cvx=False)
 
-    # Training test for MRC with log loss and threshold features.
-    def test_MRClog_with_threshold(self):
+    # Training test for CMRC with log loss.
+    def test_CMRClog(self):
         self.CMRC_training(phi='threshold', loss='log', use_cvx=False)
-
-    # Training test for MRC with log loss and linear kernel.
-    def test_MRClog_with_linear(self):
         self.CMRC_training(phi='linear', loss='log', use_cvx=False)
-
-    # Training test for MRC with log loss and gaussian kernel.
-    def test_MRClog_with_gaussian(self):
         self.CMRC_training(phi='gaussian', loss='log', use_cvx=False)
 
     # Using cvxpy
-    # Training test for MRC with 0-1 loss and threshold features.
-    def test_MRC0_1_with_threshold_cvx(self):
+    # Training test for CMRC with 0-1 loss.
+    def test_CMRC0_1_cvx(self):
         self.CMRC_training(phi='threshold', loss='0-1', use_cvx=True)
-
-    # Training test for MRC with 0-1 loss and linear kernel.
-    def test_MRC0_1_with_linear_cvx(self):
         self.CMRC_training(phi='linear', loss='0-1', use_cvx=True)
-
-    # Training test for MRC with 0-1 loss and gaussian kernel.
-    def test_MRC0_1_with_gaussian_cvx(self):
         self.CMRC_training(phi='gaussian', loss='0-1', use_cvx=True)
 
-    # Training test for MRC with log loss and threshold features.
-    def test_MRClog_with_threshold_cvx(self):
+    # Training test for CMRC with log loss.
+    def test_CMRClog_cvx(self):
         self.CMRC_training(phi='threshold', loss='log', use_cvx=True)
-
-    # Training test for MRC with log loss and linear kernel.
-    def test_MRClog_with_linear_cvx(self):
         self.CMRC_training(phi='linear', loss='log', use_cvx=True)
-
-    # Training test for MRC with log loss and gaussian kernel.
-    def test_MRClog_with_gaussian_cvx(self):
         self.CMRC_training(phi='gaussian', loss='log', use_cvx=True)
--- tests/test_MRC.py
+++ tests/test_MRC.py
@@ -19,58 +19,43 @@ class TestMRC(unittest.TestCase):
         r = np.unique(self.y).shape[0]
         clf = MRC(n_classes=r, phi=phi, loss=loss, use_cvx=use_cvx)
         clf.fit(self.X, self.y)
+        upper = clf.upper_
+        lower = clf.getLowerBound()
+        self.assertTrue(lower <= upper)
         self.assertTrue(hasattr(clf, 'is_fitted_'))
         self.assertTrue(clf.is_fitted_)
 
+        # Predict the probabilities for each class for the given instances.
+        if loss == 'log':
+            hy_x = clf.predict_proba(self.X)
+            self.assertTrue(hy_x.shape == (self.X.shape[0], r))
+            self.assertTrue(np.all(np.sum(hy_x, axis=1)==1))
+
         y_pred = clf.predict(self.X)
-        self.assertTrue(y_pred.shape == (self.X.shape[0],))
+        self.assertTrue(y_pred.shape == (self.X.shape[0], ))
 
     # Without using cvxpy
-    # Training test for MRC with 0-1 loss and threshold features.
-    def test_MRC0_1_with_threshold(self):
+    # Training test for MRC with 0-1 loss.
+    def test_MRC0_1(self):
         self.MRC_training(phi='threshold', loss='0-1', use_cvx=False)
-
-    # Training test for MRC with 0-1 loss and linear kernel.
-    def test_MRC0_1_with_linear(self):
         self.MRC_training(phi='linear', loss='0-1', use_cvx=False)
-
-    # Training test for MRC with 0-1 loss and gaussian kernel.
-    def test_MRC0_1_with_gaussian(self):
         self.MRC_training(phi='gaussian', loss='0-1', use_cvx=False)
 
-    # Training test for MRC with log loss and threshold features.
-    def test_MRClog_with_threshold(self):
+    # Training test for MRC with log loss.
+    def test_MRClog(self):
         self.MRC_training(phi='threshold', loss='log', use_cvx=False)
-
-    # Training test for MRC with log loss and linear kernel.
-    def test_MRClog_with_linear(self):
         self.MRC_training(phi='linear', loss='log', use_cvx=False)
-
-    # Training test for MRC with log loss and gaussian kernel.
-    def test_MRClog_with_gaussian(self):
         self.MRC_training(phi='gaussian', loss='log', use_cvx=False)
 
     # Using cvxpy
-    # Training test for MRC with 0-1 loss and threshold features.
-    def test_MRC0_1_with_threshold_cvx(self):
+    # Training test for MRC with 0-1 loss.
+    def test_MRC0_1_cvx(self):
         self.MRC_training(phi='threshold', loss='0-1', use_cvx=True)
-
-    # Training test for MRC with 0-1 loss and linear kernel.
-    def test_MRC0_1_with_linear_cvx(self):
         self.MRC_training(phi='linear', loss='0-1', use_cvx=True)
-
-    # Training test for MRC with 0-1 loss and gaussian kernel.
-    def test_MRC0_1_with_gaussian_cvx(self):
         self.MRC_training(phi='gaussian', loss='0-1', use_cvx=True)
 
-    # Training test for MRC with log loss and threshold features.
-    def test_MRClog_with_threshold_cvx(self):
+    # Training test for MRC with log loss.
+    def test_MRClog_cvx(self):
         self.MRC_training(phi='threshold', loss='log', use_cvx=True)
-
-    # Training test for MRC with log loss and linear kernel.
-    def test_MRClog_with_linear_cvx(self):
         self.MRC_training(phi='linear', loss='log', use_cvx=True)
-
-    # Training test for MRC with log loss and gaussian kernel.
-    def test_MRClog_with_gaussian_cvx(self):
         self.MRC_training(phi='gaussian', loss='log', use_cvx=True)
