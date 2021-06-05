5350b11c5d2daf431ea86d9e0baf5a0252a3b6b3
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-06-05 12:15:22 +0200

    updating docs 7

---
+++ docs/conf.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import warnings
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'numpydoc',
+    'sphinx_gallery.gen_gallery',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx'
+]
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+# General information about the project.
+project = u'MRCpy'
+copyright = (u'2021, Kartheek Reddy Bondugula, Santiago Mazuelas and '
+             u'Aritz Perez')
+author = (u'Kartheek Reddy Bondugula, Santiago Mazuelas and '
+          u'Aritz Perez')
+version = '0.1.0'
+release = '0.1.0'
+language = 'en'
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+todo_include_todos = True
+
+# Options for HTML output
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'MRCpydoc'
+
+# Option to only need single backticks to refer to symbols
+default_role = 'any'
+
+# Option to hide doctests comments in the documentation (like # doctest:
+# +NORMALIZE_WHITESPACE for instance)
+trim_doctest_flags = True
+
+# intersphinx configuration
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/{.major}'.format(
+        sys.version_info), None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'scikit-learn': ('https://scikit-learn.org/stable/', None)
+}
+
+
+# sphinx-gallery configuration
+sphinx_gallery_conf = {
+    # to generate mini-galleries at the end of each docstring in the API
+    # section: (see https://sphinx-gallery.github.io/configuration.html
+    # #references-to-examples)
+    'doc_module': 'MRCpy',
+    'backreferences_dir': os.path.join('generated'),
+}
+
+# generate autosummary even if no references
+autosummary_generate = True
+
+# Switch to old behavior with html4, for a good display of references,
+# as described in https://github.com/sphinx-doc/sphinx/issues/6705
+html4_writer = True
+
+
+# Temporary work-around for spacing problem between parameter and parameter
+# type in the doc, see https://github.com/numpy/numpydoc/issues/215. The bug
+# has been fixed in sphinx (https://github.com/sphinx-doc/sphinx/pull/5976) but
+# through a change in sphinx basic.css except rtd_theme does not use basic.css.
+# In an ideal world, this would get fixed in this PR:
+# https://github.com/readthedocs/sphinx_rtd_theme/pull/747/files
+def setup(app):
+    app.add_javascript('js/copybutton.js')
+    app.add_stylesheet("basic.css")
+
+
+# Remove matplotlib agg warnings from generated doc when using plt.show
+warnings.filterwarnings("ignore", category=UserWarning,
+                        message='Matplotlib is currently using agg, which is a'
+                                ' non-GUI backend, so cannot show the figure.')
