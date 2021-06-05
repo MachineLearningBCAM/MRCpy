524a9f48c1a63d4a10748dd29107094de65c453e
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-06-05 18:19:43 +0200

    resetting branch

--- MRCpy/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1.0'
-
-from MRCpy._MRC_ import _MRC_
-from MRCpy.CMRC import CMRC
-from MRCpy.MRC import MRC
-
-__all__ = ['_MRC_', 'MRC', 'CMRC']
--- MRCpy/phi/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Feature mapping functions used in MRC """
-from MRCpy.phi.phi import Phi
-from MRCpy.phi.phi_gaussian import PhiGaussian
-from MRCpy.phi.phi_linear import PhiLinear
-from MRCpy.phi.phi_random_relu import PhiRandomRelu
-from MRCpy.phi.phi_threshold import PhiThreshold
-
-__all__ = ['Phi', 'PhiGaussian', 'PhiThreshold', 'PhiLinear', 'PhiRandomRelu']
