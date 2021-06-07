f8d09fd9bdad04631e029cead038961e9b1026a8
KARTHEEKCIC <bkartheekreddy@gmail.com>
2021-06-07 16:07:43 +0200

    updating documentation

---
+++ _downloads/b21c0e0e27dbaf351e34747708b3d5d5/example4.py
@@ -0,0 +1,27 @@
+"""Simple example of using CMRC with 0-1 loss."""
+
+import numpy as np
+
+from MRCpy import CMRC
+# Import the datasets
+from MRCpy.datasets import load_mammographic
+
+
+if __name__ == '__main__':
+
+    # Loading the dataset
+    X, Y = load_mammographic(return_X_y=True)
+
+    # Fit the MRC model
+    clf = CMRC().fit(X, Y)
+
+    # Prediction
+    print('\n\nThe predicted values for the first 3 instances are : ')
+    print(clf.predict(X[:3, :]))
+
+    # Predicted probabilities
+    print('\n\nThe predicted probabilities for the first 3 instances are : ')
+    print(clf.predict_proba(X[:3, :]))
+
+    print('\n\nThe score is : ')
+    print(clf.score(X, Y))
---
+++ _sources/generated/MRCpy.phi.ThresholdPhi.ThresholdPhi.rst.txt
@@ -0,0 +1,17 @@
+﻿:mod:`MRCpy.phi.ThresholdPhi`.ThresholdPhi
+=================================================
+
+.. currentmodule:: MRCpy.phi.ThresholdPhi
+
+.. autoclass:: ThresholdPhi
+   :members:
+   :undoc-members:
+   :exclude-members: nesterov_optimization, try_solvers, set_learn_config_type, learn_config, get_all_subset_config
+   :inherited-members:
+   :special-members: __init__
+
+.. include:: MRCpy.phi.ThresholdPhi.ThresholdPhi.examples
+
+.. raw:: html
+
+    <div style='clear:both'></div>
