4e45b16faba01c26aa188ec15e345d265cd7761d
LasaiBarrenada <lasaitaleb@gmail.com>
2022-03-23 12:03:16 +0100

    gh-pages COVID

--- _downloads/9aad477d42f80a103f8fc4eea1115903/plot_upperLower.py
+++ _downloads/9aad477d42f80a103f8fc4eea1115903/plot_upperLower.py
@@ -24,6 +24,7 @@ feature of the MRCs. The results are for a
 
 # Import needed modules
 import time
+import warnings
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,6 +40,7 @@ from MRCpy.datasets import *
 
 sns.set_style("whitegrid")
 sns.set_context("paper")
+warnings.filterwarnings("ignore")
 
 
 def load_covid(norm=False, array=True):
