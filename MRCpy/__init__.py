b519fb28f7e562fbbfa6609547db616ae297cd62
C. Guerrero <57072512+clwarrior@users.noreply.github.com>
2022-07-08 10:26:56 +0200

    Change config

--- MRCpy/__init__.py
+++ MRCpy/__init__.py
@@ -1,8 +1,9 @@
 __version__ = '1.0'
 
-from MRCpy.amrc import AMRC
+
 from MRCpy.base_mrc import BaseMRC
 from MRCpy.cmrc import CMRC
 from MRCpy.mrc import MRC
+from MRCpy.amrc import AMRC
 
 __all__ = ['BaseMRC', 'MRC', 'CMRC', 'AMRC']
