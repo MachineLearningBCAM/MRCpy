221b846cb946254487f597792dec9cf8940a55a7
KARTHEEKCIC <bkartheekreddy@gmail.com>
2024-01-26 14:02:27 +0100

    adding tests for amrcs and dwgcs

---
+++ tests/test_amrc.py
@@ -0,0 +1,57 @@
+""" Unit tests for the adaptive minimax risk classifiers """
+
+import unittest
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# Import the dataset
+from MRCpy import AMRC
+from MRCpy.datasets import load_usenet2
+
+
+class TestAMRC(unittest.TestCase):
+
+    def setUp(self):
+        # Get the sample data for testing.
+        self.X, self.y = load_usenet2(with_info=False)
+
+        # Normalize data
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(X)
+
+    def AMRC_training_check(self, phi, loss):
+
+        # Simple functional check.
+        r = np.unique(self.y).shape[0]
+        n = self.X.shape[0]
+
+        Y_pred = np.zeros(n - 1)
+
+        clf = AMRC(phi=phi, loss=loss)
+
+        mistakes = 0
+        for i in range(n - 1):
+            clf.fit(self.X[i, :], self.y[i])
+            Y_pred[i] = clf.predict(self.X[i + 1, :])
+
+            upper = clf.get_upper_bound()
+            upper_accumulated = clf.get_upper_bound_accumulated()
+
+            if Y_pred[i] != Y[i + 1]:
+                mistakes += 1
+
+            accumulated_mistakes = mistakes / (i + 1)
+
+            self.assertTrue(hasattr(clf, 'is_fitted_'))
+            self.assertTrue(clf.is_fitted_)
+
+            # Predict the probabilities for each class for the given instances.
+            self.assertTrue(accumulated_mistakes <= upper_accumulated)
+
+    # Without using cvxpy
+    # Training test for AMRC.
+    def test_AMRC(self):
+        self.AMRC_training_check(phi='linear', loss='0-1')
+        self.AMRC_training_check(phi='fourier', loss='0-1')
---
+++ tests/test_dwgcs.py
@@ -0,0 +1,47 @@
+""" Unit tests for the constrained minimax risk classifiers """
+
+import unittest
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Import the dataset
+from MRCpy import DWGCS
+from MRCpy.datasets import load_comp_vs_sci
+
+class TestDWGCS(unittest.TestCase):
+
+    def setUp(self):
+        # Get the sample data for testing.
+        self.X_TrainSet, self.Y_TrainSet, self.X_TestSet, self.Y_TestSet = \
+                                        load_comp_vs_sci(with_info=False)
+
+    def DWGCS_training_check(self, phi, loss, solver):
+
+        # Simple functional check.
+        r = np.unique(self.y).shape[0]
+        clf = DWGCS(phi=phi,
+                    loss=loss,
+                    solver=solver,
+                    max_iters=1000)
+        clf.fit(self.X_TrainSet, self.Y_TrainSet, self.X_TestSet)
+        self.assertTrue(hasattr(clf, 'is_fitted_'))
+        self.assertTrue(clf.is_fitted_)
+
+        # Predict the probabilities for each class for the given instances.
+        hy_x = clf.predict_proba(self.X)
+        self.assertTrue(hy_x.shape == (self.X.shape[0], r))
+        self.assertTrue(np.all(np.sum(hy_x, axis=1)))
+
+        y_pred = clf.predict(self.X)
+        self.assertTrue(y_pred.shape == (self.X.shape[0],))
+
+    # Training test for DWGCS with 0-1 loss.
+    def test_DWGCS0_1_adam(self):
+        self.DWGCS_training_check(phi='linear', loss='0-1', solver='adam')
+        self.DWGCS_training_check(phi='fourier', loss='0-1', solver='adam')
+
+    # Training test for DWGCS with log loss.
+    def test_DWGCSlog_adam(self):
+        self.DWGCS_training_check(phi='linear', loss='log', solver='adam')
+        self.DWGCS_training_check(phi='fourier', loss='log', solver='adam')
